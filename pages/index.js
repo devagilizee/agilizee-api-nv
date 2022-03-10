b25b2752abadadc64517881f0f72046d9a237b2a
devagilizee <96501646+devagilizee@users.noreply.github.com>
2022-03-10 09:40:55 -0300

    inclusao api

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
-function Home(){
-    return <div>Home</div>
-}
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const rotaClientes = require('./routes/clientes')
+const rotaProdutos = require('./routes/produtos')
+const app = express()
 
-export default Home;
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.json())
+app.use(cors())
+app.use('/clientes', rotaClientes);
+app.use('/produtos', rotaProdutos);
+
+app.listen(3000, () => {
+    console.log('Express started at http://localhost:8000')
+})
